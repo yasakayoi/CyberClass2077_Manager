217d4cdcccc4331c13bef5cd983b9f3494b9b24d
yasakayoi <yw2434547487@gmail.com>
2019-07-08 19:46:00 +0800

    管理员端的基础界面

---
+++ app/src/main/java/com/example/cyberclass2077_manager/Fragment3.java
@@ -0,0 +1,17 @@
+package com.example.cyberclass2077_manager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment3_layout, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/cyberclass2077_manager/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.cyberclass2077_manager;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    Fragment firstLayout;
+    Fragment secondLayout;
+    Fragment thirdLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout_bottom);
+
+        initFragment();
+        BottomNavigationView navView = findViewById(R.id.bnv);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectFragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    selectFragment=firstLayout;
+                    break;
+                case R.id.navigation_dashboard:
+                    selectFragment=secondLayout;
+                    break;
+                case R.id.navigation_notifications:
+                    selectFragment=thirdLayout;
+                    break;
+                default:
+                    selectFragment=firstLayout;
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,selectFragment).commit();
+            return true;
+        }
+    };
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        BottomNavigationView navView = findViewById(R.id.bnv);
+        int id= getIntent().getIntExtra("fragment", 0);
+        switch (id) {
+            case 0: {
+                navView.setSelectedItemId(navView.getMenu().getItem(0).getItemId());
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,firstLayout).commit();
+                break;
+            }
+            case 1: {
+                navView.setSelectedItemId(navView.getMenu().getItem(1).getItemId());
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,secondLayout).commit();
+                break;
+            }
+            case 2: {
+                navView.setSelectedItemId(navView.getMenu().getItem(2).getItemId());
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,thirdLayout).commit();
+                break;
+            }
+        }
+    }
+
+    void initFragment()
+    {
+
+        firstLayout = new Fragment1();
+        secondLayout = new Fragment2();
+        thirdLayout = new Fragment3();
+    }
+}
